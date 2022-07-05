1c984e799ee4b99af04f9bc7a2db25bd3e7bd708
Denivaldo Prates <denivaldodev@gmail.com>
2022-07-05 17:15:28 -0300

    bloco dados-1 desafio-sent-bloco

---
+++ web-moderno/bloco.js
@@ -0,0 +1,11 @@
+{
+    console.log('bloco 1')
+    console.log('bloco 1')
+    console.log('bloco 1')
+}
+
+{
+    console.log('bloco 1')
+    console.log('bloco 1')
+    console.log('bloco 1')
+}
---
+++ web-moderno/dados-1.js
@@ -0,0 +1,11 @@
+var nome = "caneta";
+var qtde = 10;
+var preco = 6.5;
+var icms = 1.5;
+var precoFinal = preco + icms;
+
+console.log(nome)
+console.log(qtde) // declarando um valor literal
+console.log(preco)
+console.log(icms)
+console.log(precoFinal)
