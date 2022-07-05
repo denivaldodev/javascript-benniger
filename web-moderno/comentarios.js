7a1485ec06980a67d27c5c4c71a3bbcf3a879780
Denivaldo Prates <denivaldodev@gmail.com>
2022-07-05 16:09:14 -0300

    Bloco e Comentarios js

---
+++ web-moderno/comentarios.js
@@ -0,0 +1,13 @@
+// console.log('passo 1') comentário de uma linha
+console.log('Digite seu nome')
+console.log('Digite seu nome')
+/*console.log('Digite seu nome')
+*
+*
+*
+console.log('Digite seu nome')
+* comentario de multiplas linha
+*
+*
+*/
+console.log('Digite seu nome')
